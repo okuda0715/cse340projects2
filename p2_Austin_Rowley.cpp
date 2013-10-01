f91c4ab674082bd09c0ebe2daddb06b96e20eaff
Austin Rowley <ao.rowley@gmail.com>
2013-10-01 16:35:41 -0400

    added test cases for error code 0 and started on readRules

--- p2_Austin_Rowley.cpp
+++ p2_Austin_Rowley.cpp
@@ -13,48 +13,81 @@ enum errorCodes {
     error_code_3
 };
 
+vector<string> nonTerminals;                        //vector to hold a list of non-terminal tokens
+vector<string> terminals;                           //vector to hold a list of terminal tokens
+vector<string> rules;                               //vector to hold a list of rules
+
 void readNonTerminals(string tmp) {
-    vector<string> nonTerminals;
-    while(tmp.compare("#") != 0) {
-        for(int i = 0; i < tmp.length(); i++) {
-            if (ispunct(tmp.data()[i])) {
+    while(tmp.compare("#") != 0) {                  //loop throught until it finds a '#'
+        for(int i = 0; i < tmp.length(); i++) {     
+            if(ispunct(tmp.data()[i])) {            //checks to make sure there are no punctuation in the token
                 throw error_code_0;
             }
-            if(isdigit(tmp.data()[0])) {
+            if(isdigit(tmp.data()[0])) {            //chekcs to make sure the first character in the token is not a digit
                 throw error_code_0;
             }
         }
-        nonTerminals.push_back(tmp);
-        cin >> tmp;
+        nonTerminals.push_back(tmp);                //add the token to the vector non-terminal
+        cin >> tmp;                                 //read in the next token
     }
 }
 
 void readTerminals(string tmp) {
-    vector<string> terminals;
-    while(tmp.compare("#") != 0) {
+    while(tmp.compare("#") != 0) {                  //loop throught until it finds a '#'
         for(int i = 0; i < tmp.length(); i++) {
-            if (ispunct(tmp.data()[i])) {
+            if(ispunct(tmp.data()[i])) {            //checks to make sure there are no punctuation in the token
                 throw error_code_0;
             }
-            if(isdigit(tmp.data()[0])) {
+            if(isdigit(tmp.data()[0])) {            //chekcs to make sure the first character in the token is not a digit
                 throw error_code_0;
             }
         }
-        terminals.push_back(tmp);
-        cin >> tmp;
+        terminals.push_back(tmp);                   //add the token to the vector terminal
+        cin >> tmp;                                 //read in the next token
     }
 }
 
-int main() {
-    string tmp;
+void readRules(string tmp) {
+    rules.push_back("#");
+    rules.push_back("$");
     
-    cin >> tmp;
+    for(int i=0; i < terminals.size(); i++) {
+        rules.push_back(terminals.at(i));
+    }
+    for(int i=0; i < nonTerminals.size(); i++) {
+        rules.push_back(nonTerminals.at(i));
+    }
+    
+    
+    
+    
+    // while(tmp.compare("##") != 0) {
+    //     if(tmp.compare("->") != 0) {
+    //         rules.push_back(tmp);
+            
+    //         for(int i = 0; i < rules.size(); i++) {
+    //             if(ispunct(rules.at(i))) {            //checks to make sure there are no punctuation in the token
+    //                 throw error_code_0;
+    //             }   
+    //             if(isdigit(rules.at(0))) {            //chekcs to make sure the first character in the token is not a digit
+    //                 throw error_code_0;
+    //             }
+    //         }
+    //     }
+    // }
+        
+}
+
+int main() {
+    string tmp;     //hold the temporary token from file
+    cin >> tmp;     //read in the first token
     
     try {
-        readNonTerminals(tmp);
+        readNonTerminals(tmp);      //read the first line in the file
+        cin >> tmp;                 //read in the next token
+        readTerminals(tmp);         //read the second line in the file
         cin >> tmp;
-        readTerminals(tmp);
-                
+        readRules(tmp);
         
     } catch(errorCodes error) {
         switch (error) {
@@ -76,8 +109,11 @@ int main() {
         }
     }
     
-    // for (unsigned n=0; n<nonTerminals.size(); ++n) {
-    //     cout << nonTerminals.at( n ) << " ";
+    for (unsigned n=0; n<rules.size(); ++n) {
+        cout << rules.at(n) << " ";
+    }
+    // for (unsigned n=0; n<terminals.size(); ++n) {
+    //     cout << terminals.at(n) << " ";
     // }
     return 0;
 }
